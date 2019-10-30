ad03fe7ba141d19340cd7b7dc3613242031e6178
bilal kazmi <bilu71115@gmail.com>
2019-10-30 23:49:47 +0500

    initial commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!l:\bilu shah 711\programming life\ride of life 711\learning_django\simple_blog_react_django\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
